8e60a8359cf851bd4e28709c24d4a296144062ae
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-16 09:23:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Math Magicians</p>
+        <Calculator />
       </header>
     </div>
   );
