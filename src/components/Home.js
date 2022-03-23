9a3780e3c04e37dc6cc011641b21bcfa10df212f
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-23 12:19:15 +0300

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import '../assets/styles/home.css';
+
+const Home = () => (
+  <main className="d-flex">
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+      quae enim eligendi itaque incidunt velit, alias ipsum asperiores id
+      nihil, labore expedita dolore hic, quia facilis sunt totam vel!
+      Veritatis laudantium quasi porro odit consequatur quia, nemo illum,
+      numquam aut totam vero vitae aperiam voluptates doloremque cumque ab!
+      Cumque, aliquam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi repellat
+      odit libero earum dolorum quo inventore, non fugit sint iste neque
+      exercitationem ex accusamus consequatur, consequuntur eaque dolore,
+      commodi veniam. Ratione, expedita quis. Deserunt eaque et est,
+      blanditiis architecto ratione saepe consequuntur quia non vero?
+    </p>
+  </main>
+);
+
+export default Home;
